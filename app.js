1e9c85ecd5a8ef074f85325b1152755ca1e50259
n0b136 <50467142+N0b136@users.noreply.github.com>
2019-07-21 09:39:24 -0500

    Completed first and second sections

---
+++ app.js
@@ -0,0 +1,31 @@
+let favMovie = (movie = "The Room", name = "World") => console.log(`My name is ${name}, and ${movie} is my favorite movie.`);
+
+favMovie();
+
+let getFirstName = fullname => {
+    return fullname.split(' ', 1);
+};
+
+let getFN = fullname => fullname.split(' ', 1);
+
+let name1 = getFirstName('Elenor Rigby');
+let name2 = getFN('Ryan McCoy');
+
+console.log(name1);
+console.log(name2);
+
+let maths = (x,y) => ({
+    exponent: (x^y),
+    Product: (x*y)
+})
+let nums = maths(4,89);
+console.log(`The product of the numbers is ${nums.Product}, and the exponent is ${nums.exponent}`);
+
+let bio = (name, location, favFood) => {
+    console.log(name);
+    console.log(location);
+    console.log(favFood);
+}
+
+let myArray = ['Ryan', 'Camp Dodge', 'Sushi'];
+bio(...myArray);
